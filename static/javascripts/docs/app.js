9dce12e644306b33b6b69c7a3e9f8d9563679c90
Dev <webmax0110.dev@gmail.com>
2016-03-20 18:10:34 +1100

    Added docs app to test

---
+++ static/javascripts/docs/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class DocApp extends React.Component {
+    render() {
+        return (
+            <h1>Hello World DocApp</h1>
+        )
+    }
+}
+
+
