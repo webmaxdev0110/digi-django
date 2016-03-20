47c47223a619f0fe081664574803df560ce78784
Dev <webmax0110.dev@gmail.com>
2016-03-20 18:37:43 +1100

    Hot fix, Heroku couldn't find React

--- static/javascripts/docs/index.js
+++ static/javascripts/docs/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 import App from './app';
-var React = require('React');
+
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
