c6e04b2eff0958b17430a78bb3ce6dc01bfcd8a8
Dev <webmax0110.dev@gmail.com>
2016-03-30 11:22:37 +1100

    Update the timing to start the number counting

--- static/javascripts/pages/home/home.js
+++ static/javascripts/pages/home/home.js
@@ -142,13 +142,36 @@ var initCountingNumber = function () {
     });
 };
 
-new Waypoint({
-    element: $('.row.efficiency')[0],
-    handler: function () {
-        initCountingNumber();
-        this.destroy();
+//http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
+var isElementInViewport = function (el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
     }
-});
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+};
+
+var eMondoEfficiencySection = $('.row.efficiency');
+if (isElementInViewport(eMondoEfficiencySection)) {
+    initCountingNumber();
+} else {
+    new Waypoint({
+        element: $('.slide-controls')[0],
+        handler: function () {
+            console.log('triggered');
+            initCountingNumber();
+            this.destroy();
+        }
+    });
+
+}
 
 
 new Waypoint({
