4d7ff2962e089d703a8b0f490e682d989ba5acce
Dev <webmax0110.dev@gmail.com>
2016-04-07 21:57:03 +1000

    [env] now reflects to the right environment

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,9 +4,8 @@ var WebpackConfig = require('webpack-config');
 
 WebpackConfig.environment.setAll({
     env: function() {
-        return process.env.NODE_ENV;
+        return process.env.WEBPACK_ENV || process.env.NODE_ENV
     }
 });
 
-//module.exports = new WebpackConfig().extend('webpack/conf/[env].config.js');
-module.exports = new WebpackConfig().extend('webpack/conf/development.config.js');
+module.exports = new WebpackConfig().extend('webpack/conf/[env].config.js');
