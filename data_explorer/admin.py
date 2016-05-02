3b09104e4882208cca5ec25bbd843bc83e5f08ec
Dev <webmax0110.dev@gmail.com>
2016-05-03 09:46:50 +1000

    Added django admin to two models

--- data_explorer/admin.py
+++ data_explorer/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from data_explorer.models import (
+    AFSLicenseeEntry,
+    AFSAuthorisedRepresentative,
+)
+
+
+class AFSLicenseeEntryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'license_no',
+        'abn',
+        'commenced_date',
+        'status'
+    ]
+    list_filter = (
+        'status'
+    )
+    search_fields = (
+        'name',
+        'license_no',
+        'abn',
+    )
+    date_hierarchy = 'commenced_date'
+
+
+
+class AFSAuthorisedRepresentativeAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'license_no',
+        'licensed_by'
+    ]
+
+
+
+admin.site.register(AFSLicenseeEntry, AFSLicenseeEntryAdmin)
+admin.site.register(AFSAuthorisedRepresentative, AFSAuthorisedRepresentativeAdmin)
