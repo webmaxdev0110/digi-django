2d9ee8b37cbddf55c933fa6d9f05908128d9b1a7
Dev <webmax0110.dev@gmail.com>
2016-05-03 09:28:39 +1000

    Added core models

---
+++ core/models.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+PENDING = 0
+ADMIN_LIVE = 1
+LIVE = 2
+
+STATUS_CHOICES = (
+    (PENDING, _('Pending')),
+    (ADMIN_LIVE, _('Admin Visible')),
+    (LIVE, _('Live')),
+)
+
+class StatusModel(models.Model):
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES)
+
+    class Meta:
+        abstract = True
