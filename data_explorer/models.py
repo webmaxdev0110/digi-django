6fd316bc4167ddd5713890ef9aadd2f7a6687eb3
Dev <webmax0110.dev@gmail.com>
2016-05-09 15:36:07 +1000

    Added JusticeOfPeace model

--- data_explorer/models.py
+++ data_explorer/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import (
+    ArrayField,
+    JSONField,
+)
 from core.models import (
     TimeStampedModel,
     StatusModel,
@@ -46,3 +49,15 @@ class AFSAuthorisedRepresentative(TimeStampedModel, models.Model):
     class Meta(object):
         verbose_name = _('Australian Financial Services Authorised Representative')
         verbose_name_plural = _('Australian Financial Services Authorised Representatives')
+
+
+class JusticeOfPeace(TimeStampedModel, models.Model):
+    jp_number = models.CharField(max_length=32, blank=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    mobile_number = models.CharField(_('Contact Number'), max_length=24, blank=True)
+    phone = models.CharField(_('Contact Number'), max_length=24, blank=True)
+    phone2 = models.CharField(_('Contact Number2'), max_length=24, blank=True, help_text='After hour')
+    state = models.CharField(_('State'), blank=True, max_length=24)
+    raw = JSONField()
