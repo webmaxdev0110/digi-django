b948f485c70fbef0589bf38d0848e8eca27a043a
Dev <webmax0110.dev@gmail.com>
2016-05-10 15:14:59 +1000

    Updated mobile number cleaning script

--- data_explorer/management/commands/clean_jp_mobile_number.py
+++ data_explorer/management/commands/clean_jp_mobile_number.py
@@ -14,16 +14,17 @@ class Command(BaseCommand):
     help = 'Clean JusticeOfPeace from scraping hub'
 
     def handle(self, *args, **kwargs):
-        for jp in JusticeOfPeace.objects.exclude(Q(mobile_number='')|Q(phone='')).iterator():
+        for jp in JusticeOfPeace.objects.exclude(mobile_number='', phone='').iterator():
             mobile_number = jp.mobile_number
             changed = False
 
-            if mobile_number and re.match(r'^(?:\+?61|0)[2-478](?:[ -]?[0-9]){8}$', mobile_number):
-                pass
-            else:
-                jp.phone = mobile_number
-                jp.mobile_number = ''
-                changed = True
+            if mobile_number:
+                if re.match(r'^(?:\+?61|0)[2-478](?:[ -]?[0-9]){8}$', mobile_number):
+                    pass
+                else:
+                    jp.phone = mobile_number
+                    jp.mobile_number = ''
+                    changed = True
 
             if jp.phone and re.match(r'^(?:\+?61|0)[2-478](?:[ -]?[0-9]){8}$', jp.phone):
                 jp.mobile_number = jp.phone
