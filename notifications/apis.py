5e4154351e0e3c34e594caac1584dfb767684a99
Dev <webmax0110.dev@gmail.com>
2016-05-10 16:35:41 +1000

    Added SMS logging

--- notifications/apis.py
+++ notifications/apis.py
@@ -4,6 +4,7 @@ from notifications.vendors.plivo_sms import (
 )
 from notifications.vendors.telstra_sms import send_telstra_sms
 import logging
+import json
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -13,10 +14,13 @@ def send_sms_message(number, text, provider='plivo'):
     if provider == 'plivo':
         result = send_plivo_sms(number, text)
         SMSNotificationTransaction.objects.create({
-            'remote_id': result.message_id
+            'remote_id': result['message_id']
         })
 
     elif provider == 'telstra':
-        send_telstra_sms(number, text)
+        result = send_telstra_sms(number, text)
+        SMSNotificationTransaction.objects.create({
+            'remote_id':  json.loads(result['text'])['messageId']
+        })
     else:
         logger.warning('Unkown SMS provider {0}'.format(provider))
