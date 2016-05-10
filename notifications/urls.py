890f5459be620d6662fff08a4ed8363a01b0dc01
Dev <webmax0110.dev@gmail.com>
2016-05-10 16:28:00 +1000

    Added telstra sms callback handler

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import url
 from notifications.views import (
     plivo_sms_callback_handler,
     plivo_sms_status_callback_handler,
+    telstra_sms_callback_handler,
 )
 
 urlpatterns = [
 
     url(r'^sms_callback/plivo/$', plivo_sms_callback_handler, name='plivo_sms_callback'),
     url(r'^sms_callback/plivo/status_report$', plivo_sms_status_callback_handler, name='plivo_sms_status_callback'),
+    url(r'^sms_callback/telstra/$', telstra_sms_callback_handler, name='telstra_sms_callback'),
 ]
