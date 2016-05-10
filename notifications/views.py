5bceae939d2209bc3d1583a9f2d00cb309f88365
Dev <webmax0110.dev@gmail.com>
2016-05-10 14:34:01 +1000

    Added SMS callback endpoints

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import logging
 
-# Create your views here.
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+@csrf_exempt
+def plivo_sms_callback_handler(request):
+    logger.info('plivo_sms_callback_handler', exe_info=True, extra={
+        'request': request,
+    })
+
+@csrf_exempt
+def plivo_sms_status_callback_handler(request):
+    logger.info('plivo_sms_status_callback_handler', exe_info=True, extra={
+        'request': request,
+    })
