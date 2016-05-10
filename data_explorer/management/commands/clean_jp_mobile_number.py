a04293d9543dc929ba975286e2cacbaf44d7cdeb
Dev <webmax0110.dev@gmail.com>
2016-05-10 15:27:39 +1000

    Updated data cleaning script

--- data_explorer/management/commands/clean_jp_mobile_number.py
+++ data_explorer/management/commands/clean_jp_mobile_number.py
@@ -19,16 +19,24 @@ class Command(BaseCommand):
             changed = False
 
             if mobile_number:
-                if re.match(r'^(?:\+?61|0)[2-478](?:[ -]?[0-9]){8}$', mobile_number):
+                if mobile_number.startswith('04') or mobile_number.startswith('+614') or mobile_number.startswith('4'):
                     pass
                 else:
                     jp.phone = mobile_number
                     jp.mobile_number = ''
                     changed = True
 
-            if jp.phone and re.match(r'^(?:\+?61|0)[2-478](?:[ -]?[0-9]){8}$', jp.phone):
-                jp.mobile_number = jp.phone
-                changed = True
+            if jp.phone:
+                if jp.phone.startswith('04') or jp.phone.startswith('+614') or jp.phone.startswith('4'):
+                    jp.mobile_number = mobile_number
+                    jp.phone = ''
+                    changed = True
 
             if changed:
+                jp.save()
+
+
+        for jp in JusticeOfPeace.objects.exclude(mobile_number__isnull=True).exclude(mobile_number='').iterator():
+            if not jp.mobile_number.startswith('+'):
+                jp.mobile_number = '+61' + jp.mobile_number
                 jp.save()
