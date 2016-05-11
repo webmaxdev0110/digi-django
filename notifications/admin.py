672f663b7b5b00328aaa6265b79eff2a0c5de9bf
Dev <webmax0110.dev@gmail.com>
2016-05-11 13:02:21 +1000

    Added transaction admin interface

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from notifications.models import (
+    SMSNotificationTransaction,
+)
+
+
+
+class SMSNotificationTransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        'remote_id',
+        'status',
+        'dest_number',
+        'user_response',
+    ]
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'remote_id',
+        'dest_number',
+        'user_response',
+    )
+
+
+admin.site.register(SMSNotificationTransaction, SMSNotificationTransactionAdmin)
+
