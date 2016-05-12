8e26e6e3f5e82f3e2f30004b894296867d155e54
Dev <webmax0110.dev@gmail.com>
2016-05-12 14:14:02 +1000

    Added author information on the blog listing page

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,6 +6,18 @@ from accounts.models import User
 class EmondoUserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('date_joined', 'signup_tag_index', 'signup_form_source',)
 
+    def get_fieldsets(self, request, obj=None):
+        fieldset = super(EmondoUserAdmin, self).get_fieldsets(request, obj)
+
+        fieldset += (
+            ('Others', {
+                'fields': ('avatar', 'short_description',),
+            }),
+        )
+        return fieldset
+
+
+
 
 admin.site.register(User, EmondoUserAdmin)
 
