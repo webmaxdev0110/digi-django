cbdab3b2179b6a4d049e5166e57d5db8e403d6b9
Dev <webmax0110.dev@gmail.com>
2016-05-17 20:27:25 +0200

    RSS setup

--- cms/blog/urls.py
+++ cms/blog/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from cms.blog.views import (
     BlogDetailView,
     BlogHomeView,
+    BlogFeed
 )
 
 urlpatterns = [
+	url(r'^feed/$', BlogFeed(), name="cms_blog_feed"),
     url(r'^(?P<slug>.*)/$', BlogDetailView.as_view(), name='cms_blog'),
     url(r'^$', BlogHomeView.as_view(), name='cms_blog_home'),
 ]
