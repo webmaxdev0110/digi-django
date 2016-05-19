684d92771b2e5fdb6d2c46b47c8a4bc0c82b658f
Dev <webmax0110.dev@gmail.com>
2016-05-19 12:22:36 +1000

    Added initial form model

---
+++ forms/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.contrib.postgres.fields import JSONField
+from django.contrib.sites.models import Site
+from django.db import models
+from accounts.models import User
+from core.models import TimeStampedModel
+
+
+class FormDocument(TimeStampedModel):
+    """
+    Represents a form document created by an user
+    The form should be accessible by document owner,
+    owner's organisation account admin
+    """
+
+    slug = models.SlugField(
+        unique=True, null=True,
+        help_text='Use for short URL sharing')
+    title = models.CharField(max_length=256, default='Untitled Form')
+    uploaded_document = models.FileField(
+        null=True, help_text='The document uploaded used for populating after a form is completed')
+    user = models.ForeignKey(User, help_text='The owner of this document')
+    form_data = JSONField()      # All the form data
+    site = models.ForeignKey(Site)
+
+    def __unicode__(self):
+        return '<FormDocument: {0}>'.format(self.title[:16])
