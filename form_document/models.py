2e07fa6222b8640e3d4fb69337cc3ed28a00ff30
Dev <webmax0110.dev@gmail.com>
2016-05-20 12:34:31 +0200

    Permission classes for FormDocument added

--- form_document/models.py
+++ form_document/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.postgres.fields import JSONField
 from django.contrib.sites.models import Site
 from django.db import models
+
 from accounts.models import User
 from core.models import TimeStampedModel
 
@@ -14,14 +15,15 @@ class FormDocument(TimeStampedModel):
     owner's organisation account admin
     """
 
-    slug = models.SlugField(
-        unique=True, null=True,
-        help_text='Use for short URL sharing')
     title = models.CharField(max_length=256, default='Untitled Form')
+    slug = models.SlugField(null=True, help_text='Use for short URL sharing')
+
     uploaded_document = models.FileField(
-        null=True, help_text='The document uploaded used for populating after a form is completed')
-    user = models.ForeignKey(User, help_text='The owner of this document')
+        null=True, 
+        help_text='The document uploaded used for populating after a form is completed')
     form_data = JSONField()      # All the form data
+
+    owner = models.ForeignKey(User, help_text='The owner of this document')
     site = models.ForeignKey(Site)
 
     def __unicode__(self):
@@ -40,7 +42,6 @@ FORM_COMPLETION_STATUS = (
     (COMPLETED, _('completed')),
 )
 
-
 class FormDocumentResponse(TimeStampedModel):
     user = models.ForeignKey(
         User,
@@ -50,3 +51,36 @@ class FormDocumentResponse(TimeStampedModel):
     form_response_data = JSONField()
     status = models.CharField(max_length=100, choices=FORM_COMPLETION_STATUS)
     
+
+CAN_READ = 0
+CAN_UPDATE = 1
+
+FORM_DOCUMENT_PERMISSIONS = (
+    (CAN_READ, _('Can read form_document')),
+    (CAN_UPDATE, _('Can update form_document')),
+)
+
+class FormDocumentUserShare(models.Model):
+    """
+    FormDocumentUserShare represents sharings of form(document) with individual users
+    There're 2 cases for sharing of document.
+        - share with company
+        - share with individual users (within company or user outside company)
+    And this model represents second case.
+
+    Regarding document's permission, we don't have "delete" permission for shared users.
+    Only document owner can delete his own document.
+    """
+    form_document = models.ForeignKey(FormDocument, related_name="shares")
+    user = models.ForeignKey(User, related_name="shared_documents")
+    permission = models.SmallIntegerField(choices=FORM_DOCUMENT_PERMISSIONS, default=CAN_READ)
+
+
+class FormDocumentCompanyShare(models.Model):
+    """
+    FormDocumentCompanyShare represents first case of sharing document.
+    Relation between this model and FormDocument is OneToOneField.
+    """
+    form_document = models.OneToOneField(FormDocument, related_name="share_in_company")
+    permission = models.SmallIntegerField(choices=FORM_DOCUMENT_PERMISSIONS, default=CAN_READ)
+
