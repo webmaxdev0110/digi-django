dd400fd26ceaccc9abce9d9cddb8ee19ee4f0e02
Dev <webmax0110.dev@gmail.com>
2016-05-27 11:57:18 +1000

    Incorrect import

--- form_document/urls.py
+++ form_document/urls.py
@@ -2,12 +2,7 @@ from django.conf.urls import include, url
 
 from rest_framework.routers import DefaultRouter
 
-from msproblem.adaptive.rest import (
-    AdaptiveWorkoutViewSet,
-    MasteryHistoryCSVExportView,
-    MasteryHistoryCSVViewSet,
-    MasteryHistoryGridViewSet,
-)
+
 
 from form_document.rest import FormDocumentViewSet
 
