c3df559abf953c88be79f9dcca50df9c49218716
Dev <webmax0110.dev@gmail.com>
2016-05-27 16:28:47 +0200

    updated models inside form_document

--- form_document/models.py
+++ form_document/models.py
@@ -30,26 +30,6 @@ class FormDocument(TimeStampedModel):
         return '<FormDocument: {0}>'.format(self.title[:16])
 
 
-class FormDocumentUserShare(TimeStampedModel):
-    """
-    FormDocumentUserShare represents sharings of form(document) with individual users
-    There're 2 cases for sharing of document.
-        - share with company
-        - share with individual users
-    And this model represents second case.
-
-    """
-    form_document = models.ForeignKey(FormDocument, related_name="shares_to_users")
-    user = models.ForeignKey(User, related_name="shares_from_users")
-
-    # Send form response result automatically to FormDocument Owner
-    send_response_to_owner_automatically = models.BooleanField(default=True,
-        help_text="Send form response result to Form owner automatically")
-
-    class Meta:
-        unique_together = (('form_document', 'user'),)
-
-
 class FormDocumentCompanyShare(TimeStampedModel):
     """
     FormDocumentCompanyShare represents first case of sharing document.
@@ -65,28 +45,38 @@ class FormDocumentCompanyShare(TimeStampedModel):
         unique_together = (('form_document', 'company'),)
 
 
-class FormDocumentCompanyUserShare(TimeStampedModel):
+class FormDocumentUserShare(TimeStampedModel):
     """
-    If document is shared with company, 
-        - company manager can decide to share it all with team members,
-        - or share with specific team members
-    FormDocumentCompanyShare.share_to_all_members is for 1st case
-    And FormDocumentCompanyUserShare represents 2nd case
+    FormDocumentUserShare represents sharings of form(document) with individual users
+    There're 2 cases for sharing of document.
+        - share with company
+        - share with individual users
+    And this model represents second case.
+
     """
-    share_obj = models.ForeignKey(FormDocumentCompanyShare, related_name="shares_among_members")
-    user = models.ForeignKey(User)
+    form_document = models.ForeignKey(FormDocument, related_name="shares_to_users")
+    shared_to_user = models.ForeignKey(User, related_name="shares_from_users")
+
+    # if form is shared with company and "shared_to_user" is company member,
+    # shared_by would link to company
+    shared_by = models.ForeignKey(FormDocumentCompanyShare, null=True)
+
+    # Send form response result automatically to FormDocument Owner
+    send_response_to_owner_automatically = models.BooleanField(default=True,
+        help_text="Send form response result to Form owner automatically")
+
+    class Meta:
+        unique_together = (('form_document', 'user'),)
 
 
 FROM_OWNER = 0
 FROM_SHARED_USER = 1
-FROM_SHARED_COMPANY_ADMIN = 2
-FROM_SHARED_COMPANY_USER = 3
+FROM_SHARED_COMPANY = 2
 
 AVAILABLE_FORM_SOURCES = (
     (FROM_OWNER, _('From form owner')),
     (FROM_SHARED_USER, _('From shared user')),
-    (FROM_SHARED_COMPANY_ADMIN, _('From shared company admin')),
-    (FROM_SHARED_COMPANY_USER, _('From shared company user'))
+    (FROM_SHARED_COMPANY_ADMIN, _('From shared company')),
 )
 
 
@@ -101,7 +91,14 @@ class FormDocumentSource(TimeStampedModel):
     source = models.SmallIntegerField(default=FROM_OWNER, choices=AVAILABLE_FORM_SOURCES)
     user_share = models.ForeignKey(FormDocumentUserShare, null=True)
     company_share = models.ForeignKey(FormDocumentCompanyShare, null=True)
-    company_share_user = models.ForeignKey(FormDocumentCompanyUserShare, null=True)
+
+
+class RecipientTracking(models.Model):
+    email = models.EmailField(null=True)
+    phone = models.CharField(max_length=30, null=True)
+
+    class Meta:
+        abstract = True
 
 
 UNOPENED = 1
@@ -118,7 +115,7 @@ FORM_COMPLETION_STATUS = (
     (SUBMITTED, _('Submitted')),
 )
 
-class FormDocumentResponse(TimeStampedModel):
+class FormDocumentResponse(TimeStampedModel, RecipientTracking):
     """
     FormDocumentResponse represents Form submission per User
     When form is published, users registered to platform or anonymose users can submit form
