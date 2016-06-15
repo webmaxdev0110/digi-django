f2c829299e4c849cc1dd46f3595359a66a53636c
Dev <webmax0110.dev@gmail.com>
2016-06-15 18:14:16 +1000

    Added API to send html emails

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -1,6 +1,17 @@
 from celery.task import task
-from notifications.apis import send_sms_message
+from notifications.apis import (
+    send_sms_message,
+    send_email,
+)
+from emondo.celery import app
 
-@task
+
+
+@app.task
 def send_one_off_sms_message(number, text, provider='plivo'):
     send_sms_message(number, text, provider)
+
+
+@app.task
+def send_one_off_email(subject, to_address, text_email='', html_email='', tags=None, metadata=None):
+    send_email(subject, to_address, text_email, html_email, tags, metadata)
