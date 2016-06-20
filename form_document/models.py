033de004ad749af8b29ed19a7d171535a018429f
Dev <webmax0110.dev@gmail.com>
2016-06-20 14:49:11 +1000

    Added access code on the FormDocument model

--- form_document/models.py
+++ form_document/models.py
@@ -61,7 +61,7 @@ class FormDocument(TimeStampedModel):
         null=True
     )
     form_config = JSONField(null=True)
-
+    access_code = models.CharField(max_length=4, null=True)
     owner = models.ForeignKey(User, help_text='The owner of this document')
     site = models.ForeignKey(Site)
 
