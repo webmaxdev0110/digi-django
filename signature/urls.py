53997a884bdc5f0d6756799d6cd5adb9179865f2
Dev <webmax0110.dev@gmail.com>
2016-12-30 19:03:01 +0900

    Added blank signature app

---
+++ signature/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from signature.views import SignatureView
+
+urlpatterns = [
+    url(r'^(?P<style>.+)/(?P<text>.+)?$', SignatureView.as_view(), name='draw_signature'),
+]
