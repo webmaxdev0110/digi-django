1dd85334bbcbf0b80f0f5dc10ecad8522c41e3ad
Dev <webmax0110.dev@gmail.com>
2017-01-14 16:57:29 +0900

    Change the signature script to set the canvas width dynamically from the text width.

--- signature/views.py
+++ signature/views.py
@@ -20,7 +20,7 @@ class SignatureView(View):
         text=kwargs.get('text', 'Welcome to emondo.io')
         # Prepend a space to deal with
         # letter "J" to be drawn off canvas
-        text = ' ' + text
+        text = ' ' + text + ' '
         style_to_font = {
             'swift': 'MsSwift6.ttf',
             'lincoln': 'Lincoln8.ttf',
@@ -30,17 +30,14 @@ class SignatureView(View):
         # Render the text 4 times larger and shrink down to original size
         # to get antialias
         rendering_multiply = 4
-        c_w, c_h = canvas_size = (800 * rendering_multiply, 330 * rendering_multiply,)
-
-        image = Image.new('RGBA', canvas_size, (255, 255, 255, 0,))
-        draw = ImageDraw.Draw(image)
+        c_h = 330 * rendering_multiply
         fontsize = 10  # starting font canvas_size
         # portion of image width you want text width to be
         img_fraction = 0.7
         fonts_path = os.path.join(settings.BASE_DIR, 'static/fonts')
         font_path = os.path.join(fonts_path, font_name)
         font = ImageFont.truetype(font_path, fontsize)
-        while font.getsize(text)[0] < img_fraction * image.size[0]:
+        while font.getsize(text)[1] < img_fraction * c_h:
             # iterate until the text canvas_size is just larger than the criteria
             fontsize += 1
             font = ImageFont.truetype(font_path, fontsize)
@@ -48,8 +45,14 @@ class SignatureView(View):
         f_w, f_h = font.getsize(text)
 
         # optionally de-increment to be sure it is less than criteria
-        fontsize -= 1
-        font = ImageFont.truetype(font_path, fontsize)
+        #fontsize -= 1
+        #font = ImageFont.truetype(font_path, fontsize)
+
+        c_w = f_w + 100
+        canvas_size = (c_w, c_h)
+        image = Image.new('RGBA', canvas_size, (255, 255, 255, 0,))
+        draw = ImageDraw.Draw(image)
+
         draw.text(((c_w - f_w) / 2, (c_h - f_h) / 2), text, font=font, fill=(0,0,0,))
 
         response = HttpResponse(content_type="image/png")
