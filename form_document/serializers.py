65762d462293758500b93d224749dfb2fec63514
Dev <webmax0110.dev@gmail.com>
2017-01-15 05:36:25 +0900

    Added dummy fields to form document response end point

--- form_document/serializers.py
+++ form_document/serializers.py
@@ -81,11 +81,51 @@ class FormDocumentResponseSerializer(ModelSerializer):
     response_id = serializers.ReadOnlyField(source='pk')
     form_id = serializers.ReadOnlyField(source='form_document.pk')
     answers = serializers.JSONField()
+    completion_percent = serializers.SerializerMethodField()
+    completed_by_name = serializers.SerializerMethodField()
+    sent_channel = serializers.SerializerMethodField()
+    type = serializers.SerializerMethodField()
+    duration_seconds = serializers.SerializerMethodField()
+    contact_email = serializers.SerializerMethodField()
+    contact_phone = serializers.SerializerMethodField()
+
 
     class Meta:
         model = FormDocumentResponse
         fields = (
             'response_id',
             'form_id',
+            'created',
             'answers',
+            'completion_percent',
+            'completed_by_name',
+            'sent_channel',
+            'status',
+            'type',
+            'duration_seconds',
+            'contact_email',
+            'contact_phone',
         )
+
+
+    def get_completion_percent(self, instance):
+        pass
+
+    def get_completed_by_name(self, instance):
+        pass
+
+    def get_sent_channel(self, instance):
+        pass
+
+    def get_type(self, instance):
+        pass
+
+    def get_duration_seconds(self, instance):
+        pass
+
+    def get_contact_email(self, instance):
+        pass
+
+    def get_contact_phone(self, instance):
+        pass
+
