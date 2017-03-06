cac597ebeddbb669ca98137bbfa063e0f4c4b953
Dev <webmax0110.dev@gmail.com>
2017-03-06 14:41:41 +0900

    Added app constants

---
+++ identity_verification/constants.py
@@ -0,0 +1,17 @@
+
+class VerificationSource(object):
+    DVSPASSPORT = 0
+    DVSDRIVERLICENSE = 1
+    DVSMEDICARECARD = 2
+    AUSTRALIAN_ELECTORAL_ROLL = 3
+    AUSTRALIAN_CREDIT_AGENCY = 4
+
+
+VERIFICATION_SOURCES = (
+    (VerificationSource.DVSDRIVERLICENSE, 'DVSDriverLicense',),
+    (VerificationSource.DVSPASSPORT, 'DVSDriverLicense',),
+    (VerificationSource.DVSMEDICARECARD, 'DVSDriverLicense',),
+    (VerificationSource.AUSTRALIAN_ELECTORAL_ROLL, 'Australian Electoral Roll',),
+    (VerificationSource.AUSTRALIAN_CREDIT_AGENCY, 'Australian Credit Agency',),
+)
+
