de10fce95cd769b2f1b618c63592b0aad7db8557
Dev <webmax0110.dev@gmail.com>
2017-03-08 13:57:31 +0900

    DRY Trulioo passport response check

--- identity_verification/utils.py
+++ identity_verification/utils.py
@@ -39,18 +39,8 @@ def is_dvs_driver_license_match(trulioo_response):
 
 
 def is_passport_match(trulioo_response):
-    data_source_result_list = trulioo_response.get('Record', {}).get('DatasourceResults', [])
-    dvs_passport_result_search = filter(
-        lambda x: x['DatasourceName'] == 'DVS Passport Search',
-        data_source_result_list)
-
-    if len(dvs_passport_result_search):
-        match_result = dvs_passport_result_search[0]
-    else:
-        return False
-
-    result_data_source_fields = match_result['DatasourceFields']
-    match_keys = [
+    passport_result = extract_trulioo_response(trulioo_response, 'DVS Passport Search')
+    required_fields = [
         'Gender',
         'FirstGivenName',
         'FirstSurName',
@@ -59,16 +49,4 @@ def is_passport_match(trulioo_response):
         'DayOfBirth',
         'PassportNumber'
     ]
-
-    if len(result_data_source_fields) < len(match_keys):
-        # Trulioo may return invalid response that not all keys present
-        return False
-
-    field_results = []
-    for field_obj in result_data_source_fields:
-        if field_obj['FieldName'] in match_keys and field_obj['Status'] != 'match':
-            match_result = False
-            break
-
-    return match_result
-
+    return test_trulioo_fields_match(passport_result, required_fields)
