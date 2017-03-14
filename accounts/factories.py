5f691c0c71aeb8ce74877b0371b5a86a4331ed6b
Dev <webmax0110.dev@gmail.com>
2017-03-14 14:22:52 +0900

    Added company factory

--- accounts/factories.py
+++ accounts/factories.py
@@ -3,8 +3,10 @@ import factory
 from django.contrib.sites.models import Site
 from django.core.files.base import ContentFile
 
-from accounts.models import User
-# from form_document.models import FormDocument
+from accounts.models import (
+    User,
+    Company,
+)
 
 
 class SiteFactory(factory.DjangoModelFactory):
@@ -20,3 +22,9 @@ class UserFactory(factory.DjangoModelFactory):
 
     username = factory.sequence(lambda n: 'User <%d>' % n)
     is_active = True
+class CompanyFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Company
+    title = factory.sequence(lambda n: 'User <%d>' % n)
+    owner = factory.SubFactory(UserFactory)
+    active = True
