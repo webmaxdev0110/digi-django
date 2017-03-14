6b37d65259b3466c80185a967ff0da613585eafd
Dev <webmax0110.dev@gmail.com>
2017-03-14 14:22:37 +0900

    Added Site factory

--- accounts/factories.py
+++ accounts/factories.py
@@ -1,9 +1,17 @@
 
 import factory
+from django.contrib.sites.models import Site
 from django.core.files.base import ContentFile
 
 from accounts.models import User
 # from form_document.models import FormDocument
+
+
+class SiteFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Site
+    name = 'example'
+    domain = 'example.com'
 
 
 class UserFactory(factory.DjangoModelFactory):
