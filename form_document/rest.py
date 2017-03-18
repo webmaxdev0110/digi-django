9bfafba565f782f8f2d7522a8130cd9c852beec5
Dev <webmax0110.dev@gmail.com>
2017-03-18 18:31:47 +0900

    Added Detail serialiser for form builder to pull form

--- form_document/rest.py
+++ form_document/rest.py
@@ -89,6 +89,8 @@ class FormDocumentCreateUpdateViewSet(viewsets.ModelViewSet):
             return self.serializer_class
         elif self.action == 'create' or self.action == 'update':
             return FormDocumentCreateSerializer
+        else:
+            return FormDocumentDetailSerializer
 
 
 class FormDocumentResponseViewSet(viewsets.ModelViewSet):
