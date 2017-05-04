c16596c7b0fd6ba9d22795741f06c763b3a11711
Dev <webmax0110.dev@gmail.com>
2017-05-04 14:11:59 +0900

    Added mix panel to all public pages

--- google_analytics/templatetags/analytics_tags.py
+++ google_analytics/templatetags/analytics_tags.py
@@ -10,3 +10,12 @@ def include_google_analytics():
     return {
         'include_ga': not settings.DEBUG,
     }
+
+
+
+@register.inclusion_tag(
+    'google_analytics/elements/mixpanel.html')
+def include_mixpanel():
+    return {
+        'include_mixpanel': not settings.DEBUG,
+    }
