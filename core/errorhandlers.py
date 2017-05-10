0e54d05cb065739311197731e5d9de27297d15ac
Dev <webmax0110.dev@gmail.com>
2017-05-10 22:18:36 +0900

    Fixed 500 view set up

--- core/errorhandlers.py
+++ core/errorhandlers.py
@@ -4,7 +4,7 @@ import sys
 from django.http import (
     HttpResponseServerError,
 )
-from django.template.context_processors import static
+from django.template import loader
 from django.views.debug import technical_500_response
 
 
@@ -17,19 +17,6 @@ def handler500(request):
     if hasattr(request, 'user') and request.user.is_staff:
         return technical_500_response(request, *sys.exc_info())
 
-    try:
-        context = static(request)  # adds STATIC_URL
-    except Exception as e:
-        logging.error(e, exc_info=sys.exc_info(), extra={'request': request})
-        context = {}
+    template = loader.get_template('core/500.html')
+    return HttpResponseServerError(template.render())
 
-    try:
-        context['exception_type'] = type(sys.exc_info()[1]).__name__
-        context['exception'] = sys.exc_info()[1]
-    except Exception:
-        context['exception_type'] = 'Error'
-        context['exception'] = 'Error getting exception'
-
-    context['request'] = request
-
-    return HttpResponseServerError('500 error')
