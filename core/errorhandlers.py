6598559afbbad42c626a41441fbec68071ef1524
Dev <webmax0110.dev@gmail.com>
2017-05-13 20:43:28 +0900

    Added static url support

--- core/errorhandlers.py
+++ core/errorhandlers.py
@@ -5,6 +5,7 @@ from django.http import (
     HttpResponseServerError,
 )
 from django.template import loader
+from django.template.context_processors import static
 from django.views.debug import technical_500_response
 
 
@@ -16,6 +17,21 @@ def handler500(request):
     # Show admins a page with stacktrace
     if hasattr(request, 'user') and request.user.is_staff:
         return technical_500_response(request, *sys.exc_info())
+
+    try:
+        context = static(request)  # adds STATIC_URL
+    except Exception as e:
+        logging.error(e, exc_info=sys.exc_info(), extra={'request': request})
+        context = {}
+
+    try:
+        context['exception_type'] = type(sys.exc_info()[1]).__name__
+        context['exception'] = sys.exc_info()[1]
+    except Exception:
+        context['exception_type'] = 'Error'
+        context['exception'] = 'Error getting exception'
+
+    context['request'] = request
 
     template = loader.get_template('core/500.html')
     return HttpResponseServerError(template.render())
